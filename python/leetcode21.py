119af6d8ee5598977986ea435b79ca70becb4778
IulianCovaliuc <iulian.secops@protonmail.com>
2025-11-24 23:03:31 +0000

    Attempt At Resolving Leetcode21
    Didn't Work, need to re-study merged lists and linked lists

---
+++ python/leetcode21.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        self.list1 = list1
+        self.list2 = list2
+        self.merged_lists = merged_lists
+        merged_lists = sorted(list1 + list2)
+        return merged_lists
+    
+    
+    print(mergeTwoLists([1, 2 ,3 ], [1, 3, 4]))
+    
+
+Solution.mergeTwoLists()
